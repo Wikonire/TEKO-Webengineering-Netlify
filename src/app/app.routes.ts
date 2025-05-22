447c9216b55a034d17247087195792b161484adb
Wikonire <ni.widmer@bluewin.ch>
2025-05-22 20:46:32 +0200

    Add welcome page and improve existing styles
    Introduced a new Welcome component as the default route with its UI and basic structure. Enhanced styling in KnowTism and app components for better layout alignment and visual consistency. Updated route configurations to accommodate the Welcome component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {KnowntismComponent} from './knowntism/knowntism.component';
+import {WelcomeComponent} from './welcome/welcome.component';
 export const routes: Routes = [
   { path: 'knowtism', pathMatch: 'full', component: KnowntismComponent },
-  { path: '**', redirectTo: 'knowtism', pathMatch: 'full' },
+  { path: 'welcome', pathMatch: 'full', component: WelcomeComponent },
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
 ];
