f6622e66e95aad4d8a118a1f93a505ebbbcebfba
Wikonire <ni.widmer@bluewin.ch>
2025-05-22 20:50:46 +0200

    Update wildcard route to load WelcomeComponent directly
    Replaced the wildcard route's redirection with direct loading of the `WelcomeComponent`. This ensures a smoother and more predictable navigation experience for undefined paths.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import {WelcomeComponent} from './welcome/welcome.component';
 export const routes: Routes = [
   { path: 'knowtism', pathMatch: 'full', component: KnowntismComponent },
   { path: 'welcome', pathMatch: 'full', component: WelcomeComponent },
-  { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: WelcomeComponent },
 ];
