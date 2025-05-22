4aaefc6027e354526e79c0dc2d2a9da83483aa27
Wikonire <ni.widmer@bluewin.ch>
2025-05-22 10:33:18 +0200

    Add `cross-env` dependency to project
    `cross-env` was added to `devDependencies` to allow setting platform-independent environment variables during development and testing. This ensures better compatibility across different operating systems.

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,17 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = async () => {
+export default function bootstrap() {
   try {
     console.log('>> Bootstrap SSR gestartet');
-    await bootstrapApplication(AppComponent, config);
+    return bootstrapApplication(AppComponent, config);
   } catch (err) {
     console.error('!! SSR-Fehler beim Bootstrap:', err);
-    // Nur in CI/CD-Umgebungen relevant:
     if (typeof process !== 'undefined' && process.exit) {
       process.exit(1);
     }
+    return Promise.reject(err);
   }
-};
-
-export default bootstrap;
+}
