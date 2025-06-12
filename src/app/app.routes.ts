7d7821e8b0b79bb1b55a4ad6b1cb817776fe6508
Wikonire <ni.widmer@bluewin.ch>
2025-06-12 09:17:13 +0200

    Add ResponsiveDesignComponent with routing and tests
    Introduce ResponsiveDesignComponent with a basic template and style structure. Update app routes to include the new component and add unit tests to ensure its functionality.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {KnowntismComponent} from './knowntism/knowntism.component';
 import {WelcomeComponent} from './welcome/welcome.component';
+import {ResponsiveDesignComponent} from './responsive-design/responsive-design.component';
 export const routes: Routes = [
   { path: 'knowtism', pathMatch: 'full', component: KnowntismComponent },
   { path: 'welcome', pathMatch: 'full', component: WelcomeComponent },
+  { path: 'responsive-design', pathMatch: 'full', component: ResponsiveDesignComponent },
   { path: '**', component: WelcomeComponent },
 ];
---
+++ src/app/responsive-design/responsive-design.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsive-design',
+  imports: [],
+  templateUrl: './responsive-design.component.html',
+  styleUrl: './responsive-design.component.scss'
+})
+export class ResponsiveDesignComponent {
+
+}
