ec4e14580d0d4a8d15fe17c53783031fb6fdee52
Wikonire <ni.widmer@bluewin.ch>
2025-06-12 10:31:25 +0200

    Update titles, improve quotes, and refine responsive styles
    Adjusted titles for better clarity and alignment with purpose. Updated autism empowerment quotes to enhance content appeal and relatability. Revised SCSS for improved responsive design using rem-based units and updated breakpoints.

--- src/app/responsive-design/responsive-design.component.ts
+++ src/app/responsive-design/responsive-design.component.ts
@@ -7,14 +7,14 @@ import { Component } from '@angular/core';
 })
 export class ResponsiveDesignComponent {
   public readonly autismEmpowermentQuotes:string[] = [
-    "Ich bin nicht seltsam – ich bin eine limitierte Sonderedition.",
-    "Autistisch? Ja. Aber du bist neurotypisch und ich akzeptiere dich trotzdem!",
-    "Smalltalk? Nein, danke. Lieber Deep Talk und Snacks!",
+    "Ich bin nicht seltsam – ich bin limitierte Edition.",
+    "Autistisch? Ja klar, was hast du dagegen`? Du bist neurotypisch und ich akzeptiere dich trotzdem!",
+    "Smalltalk? Nein, danke. Lieber Deep Talk und Snacks! Und Kuchen und Popcorn! Gaaanz viel Kuchen und Popcorn!",
     "Ich bin nicht unsozial, ich bin selektiv sozial.",
-    "Mein Gehirn hat viele Tabs offen – und irgendwo läuft Musik.",
-    "Eye-Contact ist überbewertet – Kuchen hingegen nicht!",
+    "Mein Gehirn hat viele Tabs offen und irgendwo läuft Musik.",
+    "Eye-Kontakt ist überbewertet – Kuchen hingegen nicht!",
     "Ich stimme nicht zu laut, du bist nur zu sensibel.",
-    "Mein Autismus ist keine Störung, sondern ein Feature.",
+    "It's a feature!",
     "Ich ignoriere dich nicht, mein Gehirn lädt gerade noch.",
     "Kommunikation wäre einfacher, wenn du einfach Gedanken lesen könntest."
   ];
