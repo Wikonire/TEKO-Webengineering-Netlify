69cc42bc8c5fbc641bddc6a60a2db29a41e55142
Wikonire <ni.widmer@bluewin.ch>
2025-06-12 09:35:39 +0200

    Add link to Responsive Design page and new image file
    Added a new navigation link to the Responsive Design page in the header. Also included a supporting image file for the page, enhancing its visual appeal.

--- src/app/responsive-design/responsive-design.component.ts
+++ src/app/responsive-design/responsive-design.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-responsive-design',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './responsive-design.component.html',
   styleUrl: './responsive-design.component.scss'
 })
